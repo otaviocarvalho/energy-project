7132bc8fb8f2a054f2b98738c797903a8d878223
Otávio Carvalho <omcarvalho@inf.ufrgs.br>
2015-11-24 17:29:17 -0200

    Added utility scripts to launch applications from command line

---
+++ spark/energy_project/launch_cline_input_processor.sh
@@ -0,0 +1,1 @@
+sbt 'runMain energy_stream.EnergyProcessor'
---
+++ spark/energy_project/launch_cline_input_producer.sh
@@ -0,0 +1,1 @@
+sbt 'runMain energy_stream.EnergyInputProducer localhost:9092 streaming-topic 3 5 /home/omcarvalho/tcc/dataset/sample.csv'
